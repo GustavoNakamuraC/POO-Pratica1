4479f95379b0a05ff97e6d6af62ac65c754e6586
oGustav0 <gustavonakamura6@gmail.com>
2024-08-31 12:17:20 -0300

    Adicionando a classe PontuacaoTest e Pontuacao

---
+++ AtividadesTeste/src/main/java/org/example/Pontuacao.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Pontuacao {
+    private int pontuacao;
+
+    //PontuacaoTest não pede construtor
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void adicionarPontos(int pontos) {
+        pontuacao += pontos;
+    }
+
+    public void removerPontos(int pontos) {
+        if (pontuacao > pontos){
+            pontuacao -= pontos;
+        }
+    }
+}
